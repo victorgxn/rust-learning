bde1ab6b698dad7ceb38ce0d1d979e6482ef3633
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-06-25 00:13:56 +0200

    sleep

--- rust-crud-api/src/main.rs
+++ rust-crud-api/src/main.rs
@@ -7,3 +7,27 @@ use std::env;
 
 #[macro_use]
 extern crate serde_derive;
+
+
+//Model: User struct with id, name and email
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
+
+//Function to connect to the database
+const DB_URL: &str = !env("DATABASE_URL");
+
+
+//Constants
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
+const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+
+fn establish_connection() -> Result<Client, PostgresError> {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let client = Client::connect(&database_url, NoTls)?;
+    Ok(client)
+}
