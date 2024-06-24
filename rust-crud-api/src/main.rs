8e16cea382937a2dcd4a6a925c615562ba8a961c
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-06-24 23:58:41 +0200

    REST API

---
+++ rust-crud-api/src/main.rs
@@ -0,0 +1,9 @@
+
+use postgres::{Client, NoTls};
+use postgres::Error as PostgresError;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
