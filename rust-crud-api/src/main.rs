eaeaacdc5c2f3aac32f8578cea7536115d0fa655
victorgxn <liseezbusiness@gmail.com>
2024-06-25 15:01:48 +0200

    crud avances

--- rust-crud-api/src/main.rs
+++ rust-crud-api/src/main.rs
@@ -8,7 +8,6 @@ use std::env;
 #[macro_use]
 extern crate serde_derive;
 
-
 //Model: User struct with id, name and email
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -18,16 +17,46 @@ struct User {
 }
 
 //Function to connect to the database
-const DB_URL: &str = !env("DATABASE_URL");
+const DB_URL: &str = env!("DATABASE_URL");
 
 
 //Constants
-const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
-const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
 
-fn establish_connection() -> Result<Client, PostgresError> {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let client = Client::connect(&database_url, NoTls)?;
-    Ok(client)
+//Database function
+fn set_database() -> Result<(), PostgresError> {
+    //Connect to database
+    let mut client = Client::connect(DB_URL, NoTls)?;
+
+    //Create table
+    client.batch_execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL
+        )"
+    )?;
+    Ok(())
 }
+
+
+//Main function
+fn main() {
+    
+    //set database
+    if let Err(e) = set_database () {
+         println!("Error {}", e)
+         return;
+    }
+
+    //start server and print port
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    println!("Server started at port 8080");
+
+    //handle the client
+   
+}
+
+
