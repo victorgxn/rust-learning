50719374370ad311d3a135e4e9834ea3c1dbb0d9
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-06-30 15:18:51 +0200

    acabado endpoin

--- stingray/README.md
+++ stingray/README.md
@@ -1,10 +1,26 @@
-## Entrar al redis
+## Warp
 
-- Acceder al contenedor de Redis
-  docker exec -it stingray redis-cli
+- Como hacer test → https://docs.rs/warp/latest/warp/test/index.html
 
-- Listar todas las claves en Redis
-  keys \*
+## Conceptos rust
 
-- Obtener el valor asociado a una clave específica
-  get test@example.com
+## Proyecto
+
+- Serde : JSON a objeto, objeto a JSON parecido a un json_encode() y json_decode() en php
+
+## Postman / Redis
+
+### Postman
+
+- Endpoint → http://127.0.0.1:3000/alert-price || Body {JSON} : `{
+    "email": "example@example.com",
+    "code": "XYZ123"
+} `
+
+### Redis
+
+- Acceder al contenedor de Redis → docker exec -it stingray redis-cli
+
+- Listar todas las claves en Redis → keys \*
+
+- Obtener el valor asociado a una clave específica → get example@example.com
--- stingray/src/main.rs
+++ stingray/src/main.rs
@@ -1,39 +1,56 @@
+use dotenvy::dotenv;
 use redis::{AsyncCommands, Client};
 use serde::Deserialize;
+use std::env;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use warp::Filter;
 
+// Define una estructura para manejar la deserialización de los datos que vienen en formato JSON desde el cliente.
+// `Deserialize` permite que esta estructura acepte datos en formato JSON y los convierta en una instancia de `AlertPriceRequest`.
 #[derive(Deserialize)]
 struct AlertPriceRequest {
     email: String,
     code: String,
 }
 
+// La macro `tokio::main` transforma la función main en una función asíncrona, permitiendo usar `await`.
 #[tokio::main]
 async fn main() {
-    let redis_client = Client::open("redis://127.0.0.1/").expect("Invalid Redis URL");
+    // Carga las variables de entorno desde el archivo .env.
+    dotenv().ok();
+
+    // Se obtiene la URL de Redis desde las variables de entorno.
+    let redis_url = env::var("REDIS_URL").expect("REDIS_URL must be set");
+
+    // Se inicializa el cliente de Redis, se envuelve en un Mutex para garantizar acceso seguro entre threads,
+    // y luego se encapsula en un Arc para permitir que múltiples tareas accedan a él de manera concurrente.
+    let redis_client = Client::open(redis_url).expect("No ha sido posible conectarse al servidor de redis");
     let redis_client = Arc::new(Mutex::new(redis_client));
 
-    // Define the alert-price route
+    // Configuración de una ruta en Warp que acepta peticiones POST a '/alert-price'. Se encadena la definición de la ruta
+    // con varios filtros que procesan la entrada: captura el cuerpo JSON, el cliente de Redis y define el manejador de la ruta.
     let alert_price_route = warp::post()
         .and(warp::path("alert-price"))
         .and(warp::body::json())
         .and(with_redis(redis_client.clone()))
         .and_then(handle_alert_price);
 
-    // Start the warp server
+    // Inicia el servidor de Warp en el puerto 3000.
     warp::serve(alert_price_route)
         .run(([127, 0, 0, 1], 3000))
         .await;
 }
 
+// Filtro de Warp que inyecta el cliente de Redis en la cadena de manejadores de la solicitud.
 fn with_redis(
     redis_client: Arc<Mutex<Client>>,
 ) -> impl Filter<Extract = (Arc<Mutex<Client>>,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || redis_client.clone())
 }
 
+// Manejador asincrónico que procesa la solicitud POST. Utiliza el cliente de Redis para guardar información
+// y retorna una respuesta que se enviará al cliente.
 async fn handle_alert_price(
     payload: AlertPriceRequest,
     redis_client: Arc<Mutex<Client>>,
@@ -41,16 +58,16 @@ async fn handle_alert_price(
     let mut con = redis_client
         .lock()
         .await
-        .get_tokio_connection()
+        .get_multiplexed_tokio_connection()
         .await
-        .expect("Failed to connect to Redis");
+        .expect("No pudo conectarse a redis");
     let _: () = con
         .set(&payload.email, &payload.code)
         .await
-        .expect("Failed to set key in Redis");
+        .expect("La clave no se ha podido guardar con el gmail correctamente");
 
     Ok(format!(
-        "CORRECT ALERT CREATED for {} with id {}",
+        "Alerta creada para el email {} con el id de busqueda {}",
         payload.email, payload.code
     ))
 }
